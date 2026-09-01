b20e2a33575a25f13e10aa9ae2acc72c3e513929
dxn-scrlt <dx.scarlet.xn@gmail.com>
2026-09-01 07:35:42 -0400

    feat: add SPA navigation listener

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # YT AntiRadio
 
-![Version](https://img.shields.io/badge/version-0.1.0-ff0000?style=for-the-badge)
+![Version](https://img.shields.io/badge/version-0.1.1-ff0000?style=for-the-badge)
 ![Last Commit](https://img.shields.io/github/last-commit/dxn-scrlt/yt-antiradio?style=for-the-badge)
 
 ## Tech Stack
--- content.js
+++ content.js
@@ -1,7 +1,9 @@
-const [base, query] = window.location.href.split("?");
-const params = query.split("&");
-const videoParam = params[0];
-
-const url = `${base}?${videoParam}`;
-
-window.history.replaceState({}, "", url);
+window.addEventListener("yt-navigate-finish", () => {
+    const [base, query] = window.location.href.split("?");
+    const params = query.split("&");
+    const videoParam = params[0];
+    
+    const url = `${base}?${videoParam}`;
+    
+    window.history.replaceState({}, "", url);
+});
